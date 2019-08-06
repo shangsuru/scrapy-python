9532f3e3c666ee36c2979cdc704bc12a5ac761a0
shangsuru <helm.henry99@gmail.com>
2019-08-06 19:37:00 +0200

    db connection

--- ebay/ebay/pipelines.py
+++ ebay/ebay/pipelines.py
@@ -5,7 +5,50 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+import sqlite3
 
 class EbayPipeline(object):
+
+    def __init__(self):
+        self.create_connection()
+        self.create_table()
+
+    def create_connection(self):
+        self.conn = sqlite3.connect("cars.db")
+        self.curr = self.conn.cursor()
+
+    def create_table(self):
+        self.curr.execute("""DROP TABLE IF EXISTS car_listings""")
+        self.curr.execute("""CREATE TABLE car_listings(
+                        brand TEXT,
+                        car_type TEXT,
+                        color TEXT,
+                        fuel_type TEXT,
+                        gear_type TEXT,
+                        mileage INTEGER,
+                        model TEXT,
+                        price INTEGER,
+                        ps INTEGER,
+                        year_of_registration INTEGER
+                        )""")
+
+    def store_db(self, item):
+        self.curr.execute("""INSERT INTO car_listings VALUES (?,?,?,?,?,?,?,?,?,?)""", (
+                                item["brand"],
+                                item["car_type"],
+                                item["color"],
+                                item["fuel_type"],
+                                item["gear_type"],
+                                item["mileage"],
+                                item["model"],
+                                item["price"],
+                                item["ps"],
+                                item["year_of_registration"]
+                                ))
+        self.conn.commit()
+
     def process_item(self, item, spider):
+        self.store_db(item)
         return item
+
+
--- ebay/ebay/settings.py
+++ ebay/ebay/settings.py
@@ -27,7 +27,7 @@ CONCURRENT_REQUESTS = 10
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 2
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
@@ -64,9 +64,9 @@ DOWNLOAD_DELAY = 3
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'ebay.pipelines.EbayPipeline': 300,
-#}
+ITEM_PIPELINES = {
+    'ebay.pipelines.EbayPipeline': 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
